c672e7365b964aabd9a23efbb64c30e42104e85e
sporchia <print@blindchaos.net>
2017-01-02 09:44:12 -0500

    Initial TravisCI

--- tests/NoMajorGlitchesTest.php
+++ tests/NoMajorGlitchesTest.php
@@ -17,14 +17,37 @@ class NoMajorGlitchesTest extends TestCase {
 		}
 	}
 
+	protected function allItems() {
+		return Item::all()->copy();
+	}
+
 	protected function allItemsExcept(array $remove_items) {
-		$items = Item::all()->copy();
+		$items = $this->allItems();
 		foreach ($remove_items as $item) {
 			$items->removeItem($item);
 		}
 		return $items;
 	}
 
+	// Death Mountain
+	public function testDarkWorldEastDeathMountainCanNeverHaveTitansMitt() {
+		$no_mitt = $this->allItemsExcept(['TitansMitt']);
+
+		$this->assertFalse($this->world->getLocation("[cave-057-1F] Dark World Death Mountain - cave from top to bottom [top chest]")->fill(Item::get('TitansMitt'), $no_mitt));
+		$this->assertFalse($this->world->getLocation("[cave-057-1F] Dark World Death Mountain - cave from top to bottom [bottom chest]")->fill(Item::get('TitansMitt'), $no_mitt));
+	}
+
+	// Tower of Hera
+	public function testMoldormCantHaveFireRodIfLampUnobtainableWithoutIt() {
+		$no_lighting = $this->allItemsExcept(['Lamp', 'FireRod']);
+
+		$this->world->getLocation("[dungeon-D6-B1] Misery Mire - compass")->setItem(Item::get('Lamp'));
+		$this->world->getLocation("[dungeon-L3-1F] Tower of Hera - first floor")->setItem(Item::get('BigKey'));
+
+		$this->assertFalse($this->world->getLocation("Heart Container - Moldorm")->fill(Item::get('FireRod'), $no_lighting));
+	}
+
+	// Swamp Palace
 	public function testSwampPalaceHookshotCantBeInBigChestWithBigKeyInLateDungeon() {
 		$no_hookshot = $this->allItemsExcept(['Hookshot']);
 
@@ -34,6 +57,7 @@ class NoMajorGlitchesTest extends TestCase {
 		$this->assertFalse($this->world->getLocation("[dungeon-D2-B1] Swamp Palace - big chest")->fill(Item::get('Hookshot'), $no_hookshot));
 	}
 
+	// Palace of Darkness
 	public function testPalaceOfDarknessCanHaveBowIfFirstChestIsKey() {
 		$this->addCollected(['PowerGlove', 'Hammer', 'MoonPearl']);
 
@@ -48,10 +72,29 @@ class NoMajorGlitchesTest extends TestCase {
 		$this->assertFalse($this->world->getLocation("[dungeon-D1-B1] Dark Palace - turtle stalfos room")->fill(Item::get('Bow'), $no_bow));
 	}
 
-	public function testDarkWorldEastDeathMountainCanNeverHaveTitansMitt() {
-		$no_mitt = $this->allItemsExcept(['TitansMitt']);
+	// Misery Mire
+	public function testCompassRoomCantHaveLampIfFirerodUnobtainableWithoutIt() {
+		$no_lighting = $this->allItemsExcept(['Lamp', 'FireRod']);
 
-		$this->assertFalse($this->world->getLocation("[cave-057-1F] Dark World Death Mountain - cave from top to bottom [top chest]")->fill(Item::get('TitansMitt'), $no_mitt));
-		$this->assertFalse($this->world->getLocation("[cave-057-1F] Dark World Death Mountain - cave from top to bottom [bottom chest]")->fill(Item::get('TitansMitt'), $no_mitt));
+		$this->world->getLocation("Heart Container - Moldorm")->setItem(Item::get('FireRod'));
+		$this->world->getLocation("[dungeon-L3-1F] Tower of Hera - first floor")->setItem(Item::get('BigKey'));
+
+		$this->assertFalse($this->world->getLocation("[dungeon-D6-B1] Misery Mire - compass")->fill(Item::get('Lamp'), $no_lighting));
+	}
+
+	// Turtle Rock
+	public function testTurtleRockCantHaveLampPastDarkRoom() {
+		$no_lamp = $this->allItemsExcept(['Lamp']);
+
+		$this->assertFalse($this->world->getLocation("[dungeon-D7-B2] Turtle Rock - Eye bridge room [bottom left chest]")->fill(Item::get('Lamp'), $no_lamp));
+		$this->assertFalse($this->world->getLocation("[dungeon-D7-B2] Turtle Rock - Eye bridge room [bottom right chest]")->fill(Item::get('Lamp'), $no_lamp));
+		$this->assertFalse($this->world->getLocation("[dungeon-D7-B2] Turtle Rock - Eye bridge room [top left chest]")->fill(Item::get('Lamp'), $no_lamp));
+		$this->assertFalse($this->world->getLocation("[dungeon-D7-B2] Turtle Rock - Eye bridge room [top right chest]")->fill(Item::get('Lamp'), $no_lamp));
+		$this->assertFalse($this->world->getLocation("Heart Container - Trinexx")->fill(Item::get('Lamp'), $no_lamp));
+	}
+
+	public function testTurtleRockTrinexCantHaveKeys() {
+		$this->assertFalse($this->world->getLocation("Heart Container - Trinexx")->fill(Item::get('Key'), $this->allItems()));
+		$this->assertFalse($this->world->getLocation("Heart Container - Trinexx")->fill(Item::get('BigKey'), $this->allItems()));
 	}
 }
