e2fecd9f7ef24415ceed0b30db3a06c3800d0293
sporchia <print@blindchaos.net>
2017-01-07 12:31:27 -0500

    disable xdebug on travis
    * removed RNG tests (of note though, php7.0 and 7.1 have different
    results -_-)

--- tests/RandomizerTest.php
+++ tests/RandomizerTest.php
@@ -23,20 +23,6 @@ class RandomizerTest extends TestCase {
 	}
 
 	/**
-	 * @group RNG
-	 * @group swords
-	 */
-	public function testSwordsRandomizedByConfig() {
-		Config::set('alttp.test_rules.region.swordShuffle', true);
-
-		$this->randomizer->makeSeed(1337);
-		$this->assertEquals(Item::get('L4Sword'), $this->randomizer->getWorld()->getLocation("Alter")->getItem());
-		$this->assertEquals(Item::get('L3Sword'), $this->randomizer->getWorld()->getLocation("Blacksmiths")->getItem());
-		$this->assertEquals(Item::get('MasterSword'), $this->randomizer->getWorld()->getLocation("Pyramid")->getItem());
-	}
-
-	/**
-	 * @group RNG
 	 * @group swords
 	 */
 	public function testSwordsNotRandomizedByConfig() {
@@ -49,44 +35,6 @@ class RandomizerTest extends TestCase {
 	}
 
 	/**
-	 * @group RNG
-	 * @group crystals
-	 */
-	public function testCrystalsRandomizedByConfigNoCrossWorld() {
-		Config::set('alttp.test_rules.prize.crossWorld', false);
-		Config::set('alttp.test_rules.prize.shuffleCrystals', true);
-
-		$this->randomizer->makeSeed(1337);
-		$this->assertEquals(Item::get('Crystal3'), $this->randomizer->getWorld()->getLocation("Palace of Darkness Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal5'), $this->randomizer->getWorld()->getLocation("Swamp Palace Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal7'), $this->randomizer->getWorld()->getLocation("Skull Woods Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal2'), $this->randomizer->getWorld()->getLocation("Thieves Town Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal6'), $this->randomizer->getWorld()->getLocation("Ice Palace Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal1'), $this->randomizer->getWorld()->getLocation("Misery Mire Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal4'), $this->randomizer->getWorld()->getLocation("Turtle Rock Crystal")->getItem());
-	}
-
-	/**
-	 * @group RNG
-	 * @group crystals
-	 * @group pendants
-	 */
-	public function testCrystalsRandomizedByConfigCrossWorld() {
-		Config::set('alttp.test_rules.prize.crossWorld', true);
-		Config::set('alttp.test_rules.prize.shuffleCrystals', true);
-
-		$this->randomizer->makeSeed(1337);
-		$this->assertEquals(Item::get('Crystal3'), $this->randomizer->getWorld()->getLocation("Palace of Darkness Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal2'), $this->randomizer->getWorld()->getLocation("Swamp Palace Crystal")->getItem());
-		$this->assertEquals(Item::get('PendantOfPower'), $this->randomizer->getWorld()->getLocation("Skull Woods Crystal")->getItem());
-		$this->assertEquals(Item::get('PendantOfCourage'), $this->randomizer->getWorld()->getLocation("Thieves Town Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal6'), $this->randomizer->getWorld()->getLocation("Ice Palace Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal4'), $this->randomizer->getWorld()->getLocation("Misery Mire Crystal")->getItem());
-		$this->assertEquals(Item::get('Crystal5'), $this->randomizer->getWorld()->getLocation("Turtle Rock Crystal")->getItem());
-	}
-
-	/**
-	 * @group RNG
 	 * @group crystals
 	 */
 	public function testCrystalsNotRandomizedByConfigCrossWorld() {
@@ -104,7 +52,6 @@ class RandomizerTest extends TestCase {
 	}
 
 	/**
-	 * @group RNG
 	 * @group crystals
 	 */
 	public function testCrystalsNotRandomizedByConfigNoCrossWorld() {
@@ -121,37 +68,8 @@ class RandomizerTest extends TestCase {
 		$this->assertEquals(Item::get('Crystal7'), $this->randomizer->getWorld()->getLocation("Turtle Rock Crystal")->getItem());
 	}
 
-	/**
-	 * @group RNG
-	 * @group pendants
-	 */
-	public function testPendantsRandomizedByConfigNoCrossWorld() {
-		Config::set('alttp.test_rules.prize.crossWorld', false);
-		Config::set('alttp.test_rules.prize.shufflePendants', true);
-
-		$this->randomizer->makeSeed(1337);
-		$this->assertEquals(Item::get('PendantOfWisdom'), $this->randomizer->getWorld()->getLocation("Eastern Palace Pendant")->getItem());
-		$this->assertEquals(Item::get('PendantOfPower'), $this->randomizer->getWorld()->getLocation("Desert Palace Pendant")->getItem());
-		$this->assertEquals(Item::get('PendantOfCourage'), $this->randomizer->getWorld()->getLocation("Tower of Hera Pendant")->getItem());
-	}
 
 	/**
-	 * @group RNG
-	 * @group crystals
-	 * @group pendants
-	 */
-	public function testPendantsRandomizedByConfigCrossWorld() {
-		Config::set('alttp.test_rules.prize.crossWorld', true);
-		Config::set('alttp.test_rules.prize.shufflePendants', true);
-
-		$this->randomizer->makeSeed(1337);
-		$this->assertEquals(Item::get('PendantOfWisdom'), $this->randomizer->getWorld()->getLocation("Eastern Palace Pendant")->getItem());
-		$this->assertEquals(Item::get('Crystal7'), $this->randomizer->getWorld()->getLocation("Desert Palace Pendant")->getItem());
-		$this->assertEquals(Item::get('Crystal1'), $this->randomizer->getWorld()->getLocation("Tower of Hera Pendant")->getItem());
-	}
-
-	/**
-	 * @group RNG
 	 * @group pendants
 	 */
 	public function testPendantsNotRandomizedByConfigNoCrossWorld() {
@@ -165,7 +83,6 @@ class RandomizerTest extends TestCase {
 	}
 
 	/**
-	 * @group RNG
 	 * @group pendants
 	 */
 	public function testPendantsNotRandomizedByConfigCrossWorld() {
