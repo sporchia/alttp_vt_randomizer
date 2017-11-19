8ed1f2a380763c2d825a8c13cddb129a8f8fec8f
walkingeyerobot <mitch@thefoley.net>
2017-11-19 14:11:15 -0500

    home page updates (#307)
    * redo of copy on the main page
    
    * remove upload reference, provide is better
    
    * update discord link to match credits
    
    * changing back to tabs
    
    * fix discord href

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,26 +3,23 @@
 @section('content')
 <h1>What is A Link to the Past: VT Randomizer?</h1>
 <div class="well">
-	<p>The basic idea is that all item locations in the game have their items shuffled around… so for example you
-		never know what you’re going to find when you open a treasure chest! There are many item locations in the
-		game beyond treasure chests, for example overworld heart pieces and NPC’s that give you items
-		- these are all included too!</p>
-	<p>The VT Randomizer is a web based rom patcher that will allow you to make a randomized rom from one you
-		supply. It is meant to always have the newest things available as the Randomizer team develops them.</p>
-	<p>This was written and is maintained by Veetorp. A big thanks to Dessyreqt, ChristosOwen, Smallhacker and Karkat
+	<p>Venture back to Hyrule and an age of heroes, magic, and... shuffling with The Legend of Zelda: A Link to the Past Randomizer!
+		This time Ganon has shuffled all the items randomly throughout Hyrule so you'll need cunning, wit, and a little bit of luck
+		luck to find the Bow atop Death Mountain, or the Fire Rod resting silently in the library, or even the Master Sword itself
+		waiting in a chicken coup! Can you find everything you need to defeat Ganon? Time to find out!</p>
+	<p>This was written and is maintained by Veetorp. A big thanks to Dessyreqt, ChristosOwen, Smallhacker, and Karkat
 		for their work - without which none of this would have been even remotely possible!</p>
 </div>
 <h1>Where do I download this?</h1>
 <div class="well">
-	<p>The VT Randomizer is an entirely web based randomizer, There is no download client for it. That being said
-		the ROM is patched entirely in your browser. Being a web version we can get the newest features availble
-		to users as quickly as possible. Click on <em>Generate Randomized Game</em> above to get started.</p>
+	<p>The VT Randomizer is a web based ROM patcher. Simply provide your ROM of the base game and the website does the all the work,
+		giving you back a fully shuffled and guaranteed beatable ROM. With the web based ROM patcher, you can always be sure you
+		have the latest features as soon as they're released.</p>
 </div>
 <h1>Is there a community for this?</h1>
 <div class="well">
 	<p>Heck yeahs there is!</p>
-	<p>If you're not already a member, feel free to join our discord at
-		<a href="https://discord.gg/yEvqSRk" target="_blank">https://discord.gg/yEvqSRk</a>! You'll find lots
-		of helpful people there for when you get stuck :) Or just cool people to talk to, like the Developers.</p>
+	<p>Stuck on a seed? Looking for other heros to race with? Want to shower the developers with praise? Join our discord at
+		<a href="https://discord.gg/TCC6Y42" target="_blank">https://discord.gg/TCC6Y42</a>!</p>
 </div>
 @overwrite
