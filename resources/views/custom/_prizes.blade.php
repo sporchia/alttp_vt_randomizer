b12088559fc838be0a1cd88660b05e4341d39246
scatterclegge <christianlegge888@gmail.com>
2018-02-12 03:35:32 -0330

    added localforage saving + loading for prize packs

--- resources/views/custom/_prizes.blade.php
+++ resources/views/custom/_prizes.blade.php
@@ -1,19 +1,32 @@
 @section('prizepacks')
 <?php
 $prizepacks = [
-	['items' => ['Heart', 'Heart', 'Heart', 'Heart', 'Green Rupee', 'Heart', 'Heart', 'Green Rupee'], 'name' => 'Prize Pack 1'],
-	['items' => ['Blue Rupee', 'Green Rupee', 'Blue Rupee', 'Red Rupee', 'Blue Rupee', 'Green Rupee', 'Blue Rupee', 'Blue Rupee'], 'name' => 'Prize Pack 2'],
-	['items' => ['Large Magic Container', 'Small Magic Container', 'Small Magic Container', 'Blue Rupee', 'Large Magic Container', 'Small Magic Container', 'Heart', 'Small Magic Container'], 'name' => 'Prize Pack 3'],
-	['items' => ['1x Bomb', '1x Bomb', '1x Bomb', '4x Bomb', '1x Bomb', '1x Bomb', '8x Bomb', '1x Bomb'], 'name' => 'Prize Pack 4'],
-	['items' => ['5x Arrow', 'Heart', '5x Arrow', '10x Arrow', '5x Arrow', 'Heart', '5x Arrow', '10x Arrow'], 'name' => 'Prize Pack 5'],
-	['items' => ['Small Magic Container', 'Green Rupee', 'Heart', '5x Arrow', 'Small Magic Container', '1x Bomb', 'Green Rupee', 'Heart'], 'name' => 'Prize Pack 6'],
-	['items' => ['Heart', 'Faerie', 'Large Magic Container', 'Red Rupee', '8x Bomb', 'Heart', 'Red Rupee', '10x Arrow'], 'name' => 'Prize Pack 7'],
-	'pull' => ['items' => ['Green Rupee', 'Blue Rupee', 'Red Rupee'], 'name' => 'Pull Prizes'],
-	'crab' => ['items' => ['Green Rupee', 'Red Rupee'], 'name' => 'Bush Crab Prizes'],
-	'stun' => ['items' => ['Green Rupee'], 'name' => 'Stun Prize'],
-	'fish' => ['items' => ['Red Rupee'], 'name' => 'Fish Prize']
+	['items' => ['heart', 'heart', 'heart', 'heart', 'greenRupee', 'heart', 'heart', 'greenRupee'], 'name' => 'Prize Pack 1'],
+	['items' => ['blueRupee', 'greenRupee', 'blueRupee', 'redRupee', 'blueRupee', 'greenRupee', 'blueRupee', 'blueRupee'], 'name' => 'Prize Pack 2'],
+	['items' => ['largeMagic', 'smallMagic', 'smallMagic', 'blueRupee', 'largeMagic', 'smallMagic', 'heart', 'smallMagic'], 'name' => 'Prize Pack 3'],
+	['items' => ['bomb1', 'bomb1', 'bomb1', 'bomb4', 'bomb1', 'bomb1', 'bomb8', 'bomb1'], 'name' => 'Prize Pack 4'],
+	['items' => ['arrow5', 'heart', 'arrow5', 'arrow10', 'arrow5', 'heart', 'arrow5', 'arrow10'], 'name' => 'Prize Pack 5'],
+	['items' => ['smallMagic', 'greenRupee', 'heart', 'arrow5', 'smallMagic', 'bomb1', 'greenRupee', 'heart'], 'name' => 'Prize Pack 6'],
+	['items' => ['heart', 'faerie', 'largeMagic', 'redRupee', 'bomb8', 'heart', 'redRupee', 'arrow10'], 'name' => 'Prize Pack 7'],
+	'pull' => ['items' => ['greenRupee', 'blueRupee', 'redRupee'], 'name' => 'Pull Prizes'],
+	'crab' => ['items' => ['greenRupee', 'redRupee'], 'name' => 'Bush Crab Prizes'],
+	'stun' => ['items' => ['greenRupee'], 'name' => 'Stun Prize'],
+	'fish' => ['items' => ['redRupee'], 'name' => 'Fish Prize']
 ];
-$allprizes = ['Heart', 'Green Rupee', 'Blue Rupee', 'Red Rupee', '1x Bomb', '4x Bomb', '8x Bomb', 'Small Magic Container', 'Large Magic Container', '5x Arrow', '10x Arrow', 'Faerie'] ?>
+$alldrops = [
+	'heart' => 'Heart',
+	'greenRupee' => 'Green Rupee',
+	'blueRupee' => 'Blue Rupee',
+	'redRupee' => 'Red Rupee',
+	'bomb1' => '1x Bomb',
+	'bomb4' => '4x Bomb',
+	'bomb8' => '8x Bomb',
+	'smallMagic' => 'Small Magic Container',
+	'largeMagic' => 'Large Magic Container',
+	'arrow5' => '5x Arrow',
+	'arrow10' => '10x Arrow',
+	'faerie' => 'Faerie',
+]; ?>
 <div class="panel panel-success custom-prizes">
 	<div class="panel-heading panel-heading-btn">
 		<h3 class="panel-title pull-left">Prize Packs</h3>
@@ -41,9 +54,9 @@ $allprizes = ['Heart', 'Green Rupee', 'Blue Rupee', 'Red Rupee', '1x Bomb', '4x 
 					@foreach ($item['items'] as $innerkey => $inneritem)
 					<tr>
 						<td class="col-md-3">
-							<select id="prize-pack-{{ $key }}-{{ $innerkey }}" class="form-control selectpicker">
-								@foreach ($allprizes as $prize)
-								<option value="{{ $prize }}" <?php if ($inneritem == $prize) echo 'selected="selected"'; ?>>{{ $prize }}</option>
+							<select id="prize-pack-{{ $key }}-{{ $innerkey }}" class="form-control custom-drop selectpicker">
+								@foreach ($alldrops as $drop => $name)
+								<option value="{{ $drop }}" <?php if ($inneritem == $drop) echo 'selected="selected"'; ?>>{{ $name }}</option>
 								@endforeach
 							</select>
 						</td>
@@ -54,4 +67,32 @@ $allprizes = ['Heart', 'Green Rupee', 'Blue Rupee', 'Red Rupee', '1x Bomb', '4x 
 			@endforeach
 		</div>
 	</div>
+<script>
+$(function() {
+	var prize_packs = <?php echo json_encode($prizepacks); ?>;
+
+	localforage.getItem('vt.custom.prizepacks').then(function(value) {
+		if (value !== null) {
+			for (pack in value) {
+				for (item in value[pack].items) {
+					var setting = $('#prize-pack-' + pack + "-" + item);
+					setting.val(value[pack].items[item]);
+					setting.trigger('change');
+				}
+			}
+			prize_packs = value;
+		}
+
+		$('.custom-drop').on('change', function() {
+			var $this = $(this);
+			if (!$this.attr('id')) return;
+			pack = $this.attr('id').split("-")[2];
+			item = $this.attr('id').split("-")[3];
+			prize_packs[pack].items[item] = $this.val();
+			localforage.setItem('vt.custom.prizepacks', prize_packs);
+		});
+	});
+
+});
+</script>
 @overwrite
