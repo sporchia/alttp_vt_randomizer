40169348633fda20c84ca06f9a69ae41a1115cf3
Jonathan Baumgardner <Jon8RFC@users.noreply.github.com>
2018-02-20 00:31:24 -0600

    Update start.blade.php

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -26,8 +26,7 @@
 			role="button"
 			target="_blank">Join our Discord</a></div><br/>
 		<p>Join our Discord community! We've got friendly and helpful people, community event news,
-			aLttP: Randomizer updates, helpful guides, tips and tricks, and more! Come say
-			hello!</p>
+			ALttP: Randomizer updates, helpful guides, tips and tricks, and more! Come say hello!</p>
 	</div>
 </div>
 
