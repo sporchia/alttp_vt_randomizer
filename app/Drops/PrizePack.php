ebae35557751e13265ee9938a56cb92a332a744f
scatterclegge <christianlegge888@gmail.com>
2018-03-03 04:55:37 -0330

    cleanup

--- app/Drops/PrizePack.php
+++ app/Drops/PrizePack.php
@@ -1,12 +1,18 @@
 <?php namespace ALttP\Drops;
 
 /**
- * A Prize Pack is a set of droppable sprites that can drops
+ * A Prize Pack is a set of droppable sprites that can drop from killed enemies or other means
  */
  class PrizePack {
    protected $drops = [];
    protected $name;
 
+  /**
+  * Constructor for PrizePack class
+  *
+  * @param String the name of the Prize Pack
+  * @param int the number of slots the Prize Pack has
+  */
    public function __construct($name, $slots) {
      $this->name = $name;
      for ($i = 0; $i < $slots; $i++) {
@@ -15,18 +21,32 @@
      }
    }
 
+   /**
+   * Gets the name of the Prize Pack
+   *
+   * @return String
+   */
    public function getName() {
      return $this->name;
    }
 
+   /**
+   * Gets the Prize Pack Slots within the prize pack
+   *
+   * @return array
+   */
    public function getDrops() {
      return $this->drops;
    }
 
+   /**
+   * Gets the Prize Pack Slots within the prize pack that are not filled
+   *
+   * @return array
+   */
    public function getEmptyDrops() {
      return array_filter($this->drops, function($slot) {
        return !$slot->isFilled();
      });
    }
-
  }
