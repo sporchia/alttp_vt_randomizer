0e252faeb6ddc747fc62f60b94348a9f0338342d
Mitch Foley <mitch@thefoley.net>
2018-03-07 02:39:05 -0500

    updates to contribute page

--- resources/views/contribute.blade.php
+++ resources/views/contribute.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1>Contribute</h1>
 <div class="well">
-<h3>There are many ways to help out, join the Development channels on <a href="https://discord.gg/alttprandomizer" target="_blank" rel="noopener noreferrer">Discord</a> to find out how you can use your skills!</h3><br/>
+<h3>There are many ways to help out, join the Development channels on <a href="https://discord.gg/alttprandomizer" target="_blank" rel="noopener noreferrer">Discord</a>!</h3><br/>
 <div class="panel panel-info">
 	<div class="panel-heading">
 		<h3 class="panel-title">Sprite Development</h3>
@@ -12,8 +12,9 @@
 		<p>Want to see your own custom sprite take Link's place? Checkout
 			<a href="https://github.com/fatmanspanda/ALttPNG/wiki" target="_blank" rel="noopener noreferrer">
 				fatmanspanda's documentation!
-    	</a></p>
-		<p>For more information on sprite creation, check in with #sprite-dev on
+		</a></p>
+		<p>For more information on sprite creation and getting a custom sprite into ALttP:R, check in
+			with #sprite-dev on
 			<a href="https://discord.gg/alttprandomizer" target="_blank" rel="noopener noreferrer">Discord</a>!</p>
 	</div>
 </div>
@@ -24,10 +25,25 @@
 	</div>
 	<div class="panel-body">
 		<p>Interested in joining out team of restreamers, commentators, and trackers?</p>
-		<p>Well too bad because I don't know how to tell you how to do that.</p>
+		<p>Fill out this form and keep an eye on
+		<a href="https://discord.gg/alttprandomizer" target="_blank" rel="noopener noreferrer">Discord</a>!</p>
+		<p><iframe src="https://docs.google.com/forms/d/e/1FAIpQLScZc9r5sUgBSGvcG70Tn11xGyJcHgF8kupnWHVVwbA6usulnw/viewform?embedded=true" width="760" height="1100" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></p>
 	</div>
 </div>
 
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Other Contributions</h3>
+	</div>
+	<div class="panel-body">
+		<p>Have an idea for a feature? Find a bug while playing? Looking to put your technical skills
+			to work? Join the #suggestions channel on
+			<a href="https://discord.gg/alttprandomizer" target="_blank" rel="noopener noreferrer">Discord</a>
+			and introduce yourself!</p>
+		<p></p>
+	</div>
+</div>
+																						
 </div>
 
 @overwrite
