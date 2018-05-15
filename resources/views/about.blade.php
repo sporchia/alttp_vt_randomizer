366c22d5812193519093d376090ad19e7ed38687
Thomas Backmark <total@viskos.org>
2018-05-15 14:15:24 +0200

    fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,10 +6,11 @@
 	<br />
 	<p>Welcome to the Super Metroid and A Link to the Past crossover item randomizer.</p>
 	<p>This randomizer mixes Super Metroid and A Link to the Past together into one experience and will randomize both games items to any location in either game creating a new kind of multi-game challenge.</p>
+	<p>Travel between the two game can be done by using certain doors and entrances in either game, a list of those can be found in the document linked below.</p>
+	<p>For some more information and resources about this randomizer, check out <a href="https://docs.google.com/document/d/1fP-DRbuROuQ-zifwKftuV-Paoh0_GGYErLGv3VVAg7Y/edit">this document</a></p>
 	<br />
 	<p>This project is based on V29 of the <a href="http://vt.alttp.run">ALttP Randomizer</a> and the <a href="https://itemrando.supermetroid.run">Tournament SM Randomizer</a>, so being familiar with both of those will be helpful to be able to complete this.</p>
 	<p>Currently there are no selectable difficulties or options, but this will be implemented in future versions.</p>
-	<p>For some more information and resources about this randomizer, check out <a href="https://docs.google.com/document/d/1fP-DRbuROuQ-zifwKftuV-Paoh0_GGYErLGv3VVAg7Y/edit">this document</a></p>
 	<br />
 	<p>Huge credits to the <strong><a href="http://vt.alttp.run">ALttP Randomizer</a></strong> team for much of the tools used for the randomization and for the ALttP randomization patches and logic.</p>
 </div>
