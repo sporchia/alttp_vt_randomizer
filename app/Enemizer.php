1f65b6e15c1244168a071895e763ecd75e4beb8e
sporchia <print@blindchaos.net>
2018-10-18 00:10:01 -0400

    default to enemy randomization off, needed for standard

--- app/Enemizer.php
+++ app/Enemizer.php
@@ -86,7 +86,7 @@ class Enemizer {
 		$system = php_uname('s') == 'Darwin' ? 'osx' : 'linux';
 
 		$options = [
-			"RandomizeEnemies" => $this->settings['enemy'] ?? true,
+			"RandomizeEnemies" => $this->settings['enemy'] ?? false,
 			"RandomizeEnemiesType" => 3,
 			"RandomizeBushEnemyChance" => true,
 			"RandomizeEnemyHealthRange" => (bool) ($this->settings['enemy_health'] ?? false),
@@ -153,7 +153,7 @@ class Enemizer {
 			"BeesLevel" => 3,
 			"RandomizeTileTrapPattern" => true,
 			"RandomizeTileTrapFloorTile" => false,
-			"AllowKillableThief" => $this->settings['enemy'] ?? true,
+			"AllowKillableThief" => $this->settings['enemy'] ?? false,
 			"RandomizeSpriteOnHit" => false,
 			"DebugMode" => false,
 			"DebugForceEnemy" => true,
