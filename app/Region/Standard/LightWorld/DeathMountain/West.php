ab4093539a83adf8be651f339fdee6666c302ad4
Fouton <fouton@live.com>
2019-10-01 13:39:22 -0400

    v31.0.4
    - added superspeed as West DM Access

--- app/Region/Standard/LightWorld/DeathMountain/West.php
+++ app/Region/Standard/LightWorld/DeathMountain/West.php
@@ -46,25 +46,26 @@ class West extends Region
         $this->locations["Old Man"]->setRequirements(function ($locations, $items) {
             return $items->has('Lamp', $this->world->config('item.require.Lamp', 1));
         });
-
+        
         $this->locations["Ether Tablet"]->setRequirements(function ($locations, $items) {
             return $items->has('BookOfMudora') && ($items->hasSword(2)
                 || ($this->world->config('mode.weapons') == 'swordless' && $items->has('Hammer')))
                 && $this->world->getRegion('Tower of Hera')->canEnter($locations, $items);
         });
-
+        
         $this->locations["Spectacle Rock"]->setRequirements(function ($locations, $items) {
             return $items->has('MagicMirror')
                 || ($this->world->config('canBootsClip', false) && $items->has('PegasusBoots'))
                 || $this->world->config('canOneFrameClipOW', false);
         });
-
+        
         $this->can_enter = function ($locations, $items) {
             return $items->has('RescueZelda')
                 && ($items->canFly($this->world)
                     || $this->world->config('canOneFrameClipOW', false)
                     || ($this->world->config('canOWYBA', false) &&  $items->hasABottle())
                     || ($this->world->config('canBootsClip', false) && $items->has('PegasusBoots'))
+                    || ($this->world->config('canSuperSpeed', false) && $items->canSpinSpeed())
                     || ($items->canLiftRocks() && $items->has('Lamp', $this->world->config('item.require.Lamp', 1))));
         };
 
