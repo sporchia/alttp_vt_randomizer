e97f5c6b9403f0a279ef88286f6e032852a555b9
Fouton <fouton@live.com>
2019-10-03 15:39:22 -0400

    v31.0.4
    - added waterwalk/bunny revive options to lift rock, bunny revive as pearl replacement for mirrorwrap setup
    - removed burn requirement to ice palace boss for glitch + enemizer purposes

--- app/Region/Standard/IcePalace.php
+++ app/Region/Standard/IcePalace.php
@@ -138,8 +138,9 @@ class IcePalace extends Region
                     || (($this->world->config('canMirrorWrap', false) && $items->has('MagicMirror')
                         && (($items->has('MoonPearl')
                             || ($items->hasABottle() && $this->world->config('canOWYBA', false))
-                            || ($this->world->config('canBunnyRevive', false) && $items->canSpinSpeed()))
-                            && $this->world->config('canBootsClip', false) && $items->has('PegasusBoots'))
+                            || ($this->world->config('canBunnyRevive', false) && $items->canBunnyRevive()))
+                            && (($this->world->config('canBootsClip', false) && $items->has('PegasusBoots'))
+                                || ($this->world->config('canSuperSpeed', false) && $items->canSpinSpeed()))
                             || $this->world->config('canOneFrameClipOW', false))
                         && $this->world->getRegion('South Dark World')->canEnter($locations, $items)));
         };
