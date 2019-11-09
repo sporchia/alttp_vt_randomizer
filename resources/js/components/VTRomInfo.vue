4b6d519892e2915a2cda6482a59f3a1f97aa907e
Thomas Prescott <tcprescott@gmail.com>
2019-11-09 10:56:40 -0600

    merge old and new spoilers flag

--- resources/js/components/VTRomInfo.vue
+++ resources/js/components/VTRomInfo.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <div
-      v-if="rom.spoiler.meta.spoilers_ongen==true"
-      class="spoiler-warning"
-    >{{ $t('rom.info.spoilerwarning') }}</div>
-	  <div
-      v-if="rom.spoilers=='generate'"
+      v-if="rom.spoiler.meta.spoilers_ongen==true || rom.spoilers=='generate'"
       class="spoiler-warning"
     >{{ $t('rom.info.spoilerwarning') }}</div>
     <div v-if="rom.logic">{{ $t('rom.info.logic') }}: {{ rom.logic }}</div>
