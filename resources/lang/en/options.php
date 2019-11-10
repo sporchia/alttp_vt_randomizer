ea66dbc335c0bbce4c2eed7d44d11259b23dae1b
Thomas Prescott <tcprescott@gmail.com>
2019-11-10 10:40:08 -0600

    Reword the advanced item placement to be less awkward.

--- resources/lang/en/options.php
+++ resources/lang/en/options.php
@@ -63,7 +63,7 @@ return [
                 [
                     'header' => __('randomizer.item_placement.options.advanced'),
                     'content' => [
-                        'This setting is aimed at experienced players. The intention of this setting is to maximize glitchless item placement reach. However one exception is made to prevent navigation through dark rooms, except for rooms that have torches that one may light with the Fire Rod. No other consideration is given to the obscurity of item placements or the level of execution required to access locations. The expectation is a player choosing this setting is decently familiar and practiced with the original game.',
+                        'This setting is aimed at experienced players. The intention of this setting is to maximize glitchless item placement reach. However one exception is made to the prevention of navigating through dark rooms: Dark room navigation will never be required, except in rooms that contain torches which can be lit with the Fire Rod. No other consideration is given to the obscurity of item placements or the level of execution required to access locations. The expectation is a player choosing this setting is decently familiar and practiced with the original game.',
                     ],
                 ],
             ],
