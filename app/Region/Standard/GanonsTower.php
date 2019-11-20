1a9685e99a0087ccff1af712b5e5d6ccd09fb1a4
Michael Kunze <erzengelmichael@live.de>
2019-11-20 17:34:10 -0500

    Fixing Can Entry for DW WDM
    Changed Locations, were it makes sense, to use that canEnter instead of West Death Mountain for Clarification

--- app/Region/Standard/GanonsTower.php
+++ app/Region/Standard/GanonsTower.php
@@ -408,7 +408,7 @@ class GanonsTower extends Region
                         || ((($this->world->config('canBootsClip', false) && $items->has('PegasusBoots'))
                             || ($this->world->config('canSuperSpeed', false) && $items->has('PegasusBoots')
                                 && $items->has('Hookshot')))
-                            && $this->world->getRegion('West Death Mountain')->canEnter($locations, $items))))
+                            && $this->world->getRegion('West Dark World Death Mountain')->canEnter($locations, $items))))
                     || ($this->world->config('canOneFrameClipOW', false)
                         && ($this->world->config('canDungeonRevive', false) || $items->has('MoonPearl')
                             || ($this->world->config('canOWYBA', false) && $items->hasBottle()))));
