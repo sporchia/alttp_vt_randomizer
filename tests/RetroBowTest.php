8dedf18f1d247dc3a7a436be0c9e1c7720cdf53a
Michael Kunze <erzengelmichael@live.de>
2019-12-03 22:48:18 -0500

    Update Retro Unit Tests
    Updating Retro Unit Tests to reflect that 2 Progressive Bows don't give shootable Arrows.
    
    Also apparantly mixed tabs and spaces.. :eyes:

--- tests/RetroBowTest.php
+++ tests/RetroBowTest.php
@@ -16,10 +16,10 @@ class RetroBowTest extends TestCase
         parent::setUp();
         $this->world = World::factory('retro', ['difficulty' => 'test_rules', 'logic' => 'NoGlitches']);
         $this->addCollected(['RescueZelda', 'Lamp', 'BigKeyP1', 'MoonPearl',
-			'UncleSword', 'ProgressiveGlove', 'ProgressiveGlove', 'Flute', 'Hookshot', 'Hammer', 'FireRod',
+            'UncleSword', 'ProgressiveGlove', 'ProgressiveGlove', 'Flute', 'Hookshot', 'Hammer', 'FireRod',
             'Crystal1', 'Crystal2', 'Crystal3', 'Crystal4', 'Crystal5', 'Crystal6', 'Crystal7',
-			'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'BigKeyD1',
-			'KeyA2', 'KeyA2', 'KeyA2', 'KeyA2', 'BigKeyA2']);
+            'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'KeyD1', 'BigKeyD1',
+            'KeyA2', 'KeyA2', 'KeyA2', 'KeyA2', 'BigKeyA2']);
         $this->collected->setChecksForWorld($this->world->id);
     }
 
@@ -53,35 +53,43 @@ class RetroBowTest extends TestCase
     {
         return [
             ["Eastern Palace - Boss", false, ['ProgressiveBow']],
-            ["Eastern Palace - Boss", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Eastern Palace - Boss", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Eastern Palace - Boss", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Eastern Palace - Boss", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Palace of Darkness - Boss", false, ['ProgressiveBow']],
-            ["Palace of Darkness - Boss", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - Boss", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - Boss", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Palace of Darkness - Boss", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Palace of Darkness - The Arena - Ledge", false, ['ProgressiveBow']],
-            ["Palace of Darkness - The Arena - Ledge", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - The Arena - Ledge", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - The Arena - Ledge", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Palace of Darkness - The Arena - Ledge", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Palace of Darkness - Map Chest", false, ['ProgressiveBow']],
-            ["Palace of Darkness - Map Chest", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - Map Chest", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Palace of Darkness - Map Chest", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Palace of Darkness - Map Chest", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Ganon's Tower - Mini Helmasaur Room - Left", false, ['ProgressiveBow']],
-            ["Ganon's Tower - Mini Helmasaur Room - Left", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Mini Helmasaur Room - Left", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Mini Helmasaur Room - Left", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Ganon's Tower - Mini Helmasaur Room - Left", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Ganon's Tower - Mini Helmasaur Room - Right", false, ['ProgressiveBow']],
-            ["Ganon's Tower - Mini Helmasaur Room - Right", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Mini Helmasaur Room - Right", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Mini Helmasaur Room - Right", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Ganon's Tower - Mini Helmasaur Room - Right", true, ['ProgressiveBow', 'ShopArrow']],
 
             ["Ganon's Tower - Pre-Moldorm Chest", false, ['ProgressiveBow']],
-            ["Ganon's Tower - Pre-Moldorm Chest", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Pre-Moldorm Chest", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Pre-Moldorm Chest", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Ganon's Tower - Pre-Moldorm Chest", true, ['ProgressiveBow', 'ShopArrow']],
-			
+            
             ["Ganon's Tower - Moldorm Chest", false, ['ProgressiveBow']],
-            ["Ganon's Tower - Moldorm Chest", true, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Moldorm Chest", false, ['ProgressiveBow', 'ProgressiveBow']],
+            ["Ganon's Tower - Moldorm Chest", true, ['ProgressiveBow', 'SilverArrowUpgrade']],
             ["Ganon's Tower - Moldorm Chest", true, ['ProgressiveBow', 'ShopArrow']],
         ];
     }
