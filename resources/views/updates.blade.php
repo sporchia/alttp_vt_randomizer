33885d50056f9d9238b8a398f3dab405ba719472
Thomas Prescott <tcprescott@gmail.com>
2020-01-18 21:16:52 -0600

    Add addition of daily API endpoint to change log

--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -26,6 +26,7 @@
         <li>Added additional checks to ensure that the logic provides some offensive capability in enemy kill rooms if the enemy health setting is something other than "Default."</li>
         <li>Customizer: Enemizer options (boss shuffle, enemy shuffle, enemy damage, and enemy health) are now available.  Customizing boss locations is not yet supported.</li>
         <li>Crowd Control: Properly remove the Bug Catching Net from the Crowd Control item pool.  Blue mail armor upgrade is also available again in Crowd Control.</li>
+        <li>Added API endpoint for retrieving the hash and day of the latest daily (/api/daily).</li>
     </ul>
 </div>
 
