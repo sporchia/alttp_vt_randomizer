3292b5e8b42c013195097b8059432f55520c1d49
Thomas Prescott <tcprescott@gmail.com>
2020-05-31 17:01:32 -0500

    A *very* jank workaround for swordless entrance.
    This will ensure that entrance games have both bows available, due to an issue with Python 3.5.  Once the server is updated, or entrance is fixed, this should be removed.

--- app/World.php
+++ app/World.php
@@ -953,6 +953,16 @@ abstract class World
                 }
             }
 
+            // Workaround for a json-ordering issue in Python 3.5 and earlier.
+            // Remove this once Python is upgraded to 3.6 or later.
+            if ($this->config('mode.weapons') === 'swordless') {
+                $rom->setLimitProgressiveBow(
+                    2,
+                    Item::get($this->config('item.overflow.replacement.Bow', 'TwentyRupees2'), $this)->getBytes()[0]
+                );
+            }
+            // End of workaround
+
             if ($save) {
                 $this->saveSeedRecord();
 
