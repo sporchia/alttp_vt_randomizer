618b1bf62aa208b994ef84c46a18789d84d7f16f
Thomas Prescott <tcprescott@gmail.com>
2020-11-21 12:35:10 -0600

    fix updates

--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -10,8 +10,7 @@
             <li>Indoor music will now properly fade to half volume. (Patch by qwertymodo)</li>
             <li>Fixes a rare issue with specific SNES consoles and custom shops causing VRAM corruption or crashes.</li>
             <li>Fixes a very rare issue where the game may crash on a Save and Quit.</li>
-            <li>Fixes an issue where dying in the high stakes chest game in the Lost Woods results in respawning at the pyramid,
-                regardless of the status of Agahnim 1 or the Magic Mirror. (Thank you Catobat for the report and fix).</li>
+            <li>Fixes an issue where dying in the high stakes chest game in the Lost Woods results in respawning at the pyramid, regardless of the status of Agahnim 1 or the Magic Mirror. (Thank you Catobat for the report and fix).</li>
             <li>The Village of Outcasts chest game is no longer incrementing the checked locations counter on subsequent plays of the game. (Patch by CaitSith2)</li>
             <li>New chest key counters exist in the SRAM that may be read by auto-trackers. (Patch by Aerinon)</li>
             <li>The left side of Swamp Palace will now remain flooded if the key has been collected. (Patch by CaitSith2)</li>
